f2f98655a36b38ce8a0e7541638284dbfb2ac2d1
jakobottar <jakobottar@pm.me>
2021-12-10 14:07:50 -0700

    new approach

--- NeuralNetwork/main.py
+++ NeuralNetwork/main.py
@@ -2,11 +2,12 @@ import nn
 import numpy as np
 
 net = nn.NeuralNetwork([
-    nn.FCLayer(in_channels = 2, out_channels = 2, activation_function = 'sigmoid'),
-    nn.FCLayer(in_channels = 2, out_channels = 2, activation_function = 'sigmoid'),
-    nn.FCLayer(in_channels = 2, out_channels = 1, activation_function = 'identity')
+    nn.FCLayer(in_channels = 2, out_channels = 2, activation_function = 'sigmoid'), # input
+    nn.FCLayer(in_channels = 2, out_channels = 2, activation_function = 'sigmoid'), # hidden
+    nn.FCLayer(in_channels = 2, out_channels = 1, activation_function = 'identity', include_bias=False) # output
 ])
 
-y = net.forward(np.array([1,1]))
-print(y)
-net.backward(y, 1)
+y, A = net.forward(np.array([1, 1,1]))
+print(y, A)
+# net.backward(y, 1)
+# print(net.W)
--- NeuralNetwork/nn.py
+++ NeuralNetwork/nn.py
@@ -1,5 +1,5 @@
+# I got some inspiration from https://www.pyimagesearch.com/2021/05/06/backpropagation-from-scratch-with-python/ but the code is my own. 
 import numpy as np
-
 
 class sigmoid:
     def __call__(self, x: float) -> float:
@@ -7,7 +7,7 @@ class sigmoid:
         sig = 1 / (1 + z)
         return sig
 
-    def deriv(x):
+    def deriv(self, x):
         z = np.exp(-x)
         sig = 1 / (1 + z)
         return sig * (1 - sig)
@@ -19,49 +19,21 @@ class identity:
     def deriv(x):
         return 1
 
-class Node:
-    def __init__(self, weights, activation_function) -> None:
-        self.activation_function = activation_function
-        self.weights = weights
-
-    def eval(self, x):
-        return self.activation_function(np.dot(self.weights, x))
-
-    # zs is the set of zs that the function recieved in the forward pass
-    def deriv(self, from_child, zs):
-        grads = np.zeros_like(self.weights)
-        to_parent = np.zeros_like(self.weights) 
-        dsigmads = self.activation_function.deriv(np.dot(self.weights, zs))
-
-        for j, c in enumerate(from_child):          
-            for i, w in enumerate(self.weights):
-                grads[i] += c * dsigmads * zs[i]
-                to_parent[i] += c * dsigmads * w
-
-            print(grads)
-            to_parent = from_child * dsigmads * self.weights
-            return to_parent
-
 class FCLayer:
-    def __init__(self, in_channels, out_channels, activation_function):
-        self.nodelist = []
+    def __init__(self, in_channels, out_channels, activation_function, include_bias = True):
         self.in_channels = in_channels
         self.out_channels = out_channels
         if activation_function == 'sigmoid': self.activation_function = sigmoid()
         elif activation_function == 'identity': self.activation_function = identity()
         else: print("something broke")
-        self.layer = self._make_layer()
-
-    def _make_layer(self):
-        for i in range(self.out_channels):
-            node = Node(np.zeros((self.in_channels+1), dtype=float), self.activation_function)
-            self.nodelist.append(node)
+        
+        if include_bias:
+            self.layer_weights = np.zeros((self.in_channels+1, self.out_channels+1))
+        else:
+            self.layer_weights = np.zeros((self.in_channels+1, self.out_channels))
     
     def eval(self, x):
-        res = np.array([1])
-        for n in self.nodelist:
-            res = np.append(res, n.eval(x))
-        return res
+        return self.activation_function(np.dot(x, self.layer_weights))
     
     def deriv(self, prev, zs):
         next = []
@@ -74,18 +46,53 @@ class NeuralNetwork:
         self.layers = layers
 
     def forward(self, x): 
-        zs = [x]
-        x = np.append(1, x)
-        for l in self.layers:
-            x = l.eval(x)
-            zs.append(x)
-        res = x[1:]
-        if len(res) == 1: return res[0], zs
-        return res, zs # this is a bit messy, ideally we'd use some kind of class here
+        A = [np.atleast_2d(x)]
 
-    def backward(self, pred, target):
-        # we'll just use square loss here, no choice of loss function for now
-        zs = pred[1]
-        dLdy = np.array([pred[0] - target])
-        for i in range(len(self.layers)-1, -1, -1):
-            print(i)
+        for l in range(len(self.layers)):
+            out = self.layers[l].eval(A[l])
+            A.append(out)
+
+        return A[-1], A
+
+    def backward(self, A, y, alpha = 0.01):
+        # BACKPROPAGATION
+        # the first phase of backpropagation is to compute the
+        # difference between our *prediction* (the final output
+        # activation in the activations list) and the true target
+        # value
+        error = A[-1] - y
+
+        # from here, we need to apply the chain rule and build our
+        # list of deltas 'D'; the first entry in the deltas is
+        # simply the error of the output layer times the derivative
+        # of our activation function for the output value
+        activ = sigmoid()
+        D = [error * activ.deriv(A[-1])]
+
+        # once you understand the chain rule it becomes super easy
+        # to implement with a 'for' loop -- simply loop over the
+        # layers in reverse order (ignoring the last two since we
+        # already have taken them into account)
+        for layer in np.arange(len(A) - 2, 0, -1):
+            # the delta for the current layer is equal to the delta
+            # of the *previous layer* dotted with the weight matrix
+            # of the current layer, followed by multiplying the delta
+            # by the derivative of the nonlinear activation function
+            # for the activations of the current layer
+            delta = D[-1].dot(self.W[layer].T)
+            delta = delta * activ.deriv(A[layer])
+            D.append(delta)
+    
+          # since we looped over our layers in reverse order we need to
+        # reverse the deltas
+        D = D[::-1]
+
+        # WEIGHT UPDATE PHASE
+        # loop over the layers
+        for layer in np.arange(0, len(self.W)):
+            # update our weights by taking the dot product of the layer
+            # activations with their respective deltas, then multiplying
+            # this value by some small learning rate and adding to our
+            # weight matrix -- this is where the actual "learning" takes
+            # place
+            self.W[layer] += -alpha * A[layer].T.dot(D[layer])
